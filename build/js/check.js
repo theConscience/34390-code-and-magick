8b436b86cae7aa76402017383f4778c46b3b8323
theConscience <vinton.mail@gmail.com>
2016-03-24 12:49:47 +0300

    commit2
    remove console.log's

--- build/js/check.js
+++ build/js/check.js
@@ -16,12 +16,8 @@ var getMessage = function(a, b) {
     });
     return "Я прошёл " + sumItemsA + " шагов";
   } else if (a instanceof Array && b instanceof Array) {
-    console.log('AHAAA, youre here 2!');
     var sumOfMultipliedItems = 0;
-    console.log(a, a.length);
-    console.log(b, b.length);
     if (a.length > b.length) {
-      console.log('a > b');
       for (var i = 0; i < a.length; i++) {
         if (i < b.length) {
           sumOfMultipliedItems += a[i]*b[i];
@@ -30,7 +26,6 @@ var getMessage = function(a, b) {
         }
       }
     } else {  // для случаев, когда b >= a
-      console.log('b >= a');
       for (var i= 0; i < b.length; i++) {
         if (i < a.length) {
           sumOfMultipliedItems += b[i]*a[i];
@@ -41,5 +36,4 @@ var getMessage = function(a, b) {
     }
     return "Я прошёл " + sumOfMultipliedItems + " метров";
   }
-
 };
